ef7f2e92808928060e61379640c70b0ffb026a5c
RAHAMAN <RAHAMAN@DESKTOP-PFLI2P9>
2023-11-26 14:42:06 +0530

    commit and push Collections projects

---
+++ src/queueInterface/Sample2.java
@@ -0,0 +1,17 @@
+  package queueInterface;
+
+import java.util.PriorityQueue;
+
+public class Sample2 {
+	public static void main(String[] args) {
+		PriorityQueue e= new PriorityQueue();
+		e.add("Dinga");
+		e.add("Linga");
+		e.add("Minga");
+		e.add("Singa");
+		e.add("Tinga");
+		e.add("JUnga");
+		System.out.println(e);
+	}
+
+}
---
+++ src/queueInterface/sample.java
@@ -0,0 +1,17 @@
+package queueInterface;
+
+import java.util.PriorityQueue;
+
+public class sample {
+	public static void main(String[] args) {
+		PriorityQueue e=new PriorityQueue();
+		e.add(90);
+		e.add(12);
+		e.add(2);
+		e.add(3);
+		e.add(1);
+		e.add(88);
+		System.out.println(e);
+	}
+
+}
